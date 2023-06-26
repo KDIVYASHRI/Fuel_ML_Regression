20c25dd3170394b10ab750fdc1dea96d0087ee95
KDIVYASHRI <137698577+KDIVYASHRI@users.noreply.github.com>
2023-06-26 16:18:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def prediction(Store, Holiday, Temperature, Fuel_Price, CPI, Unemployment, Day, 
 def main():
     # front end elements of the web page
     html_temp = """
-    <div style ="background-color:yellow;padding:13px">
+    <div style ="background-color:orange;padding:13px">
     <h1 style ="color:black;text-align:center;"> divya @ Pragyan AI and FUEL - Walmart Sale Prediction ML App casestudy</h1>
     </div>
     """
